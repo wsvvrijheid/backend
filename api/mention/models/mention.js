c98d4f77df73fb61fdbbcb9a198dbc3672690c86
7alip <talipaltas@gmail.com>
2021-10-24 19:55:32 +0200

    fix: 🐛 update mention user_data after create

--- api/mention/models/mention.js
+++ api/mention/models/mention.js
@@ -5,4 +5,14 @@
  * to customize this model
  */
 
-module.exports = {}
+module.exports = {
+  lifecycles: {
+    afterCreate(result) {
+      strapi.services.twitter.lookupUser(result.username).then(data => {
+        strapi
+          .query('mention')
+          .update({ username: result.username }, { user_data: data[0] })
+      })
+    },
+  },
+}
