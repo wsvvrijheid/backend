6c348f0de818eb860147883e1f6ca61a76dbdef9
7alip <talipaltas@gmail.com>
2021-11-07 23:03:06 +0100

    feat: ✨ mention search from twitter

--- config/hook.js
+++ config/hook.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   settings: {
     twitter: {
       enabled: true, // set to enable the hook
-      debug: true, // to log the data
+      debug: false, // to log the data
       consumer_key: env('TWITTER_CONSUMER_KEY'),
       consumer_secret: env('TWITTER_CONSUMER_SECRET'),
       access_token_key: env('TWITTER_ACCESS_TOKEN'),
