971a0c4d3a6853b6f4ef3e25e6e391a032931078
7alip <talipaltas@gmail.com>
2021-12-20 10:57:40 +0100

    refactor: ♻️ cron job

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,12 +1,6 @@
 const { sanitizeEntity } = require('strapi-utils')
 
 module.exports = {
-  /**
-   * Cron job with timezone example.
-   * Every Monday at 1am for Asia/Dhaka timezone.
-   * List of valid timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List
-   */
-
   '*/15 * * * *': {
     task: async () => {
       const woeids = {
@@ -15,20 +9,27 @@ module.exports = {
         nl: 23424909,
       }
 
-      Object.entries(woeids).map(async ([locale, id]) => {
-        const result = await strapi.hook.twitter.getTrends(id)
+      const entities = await Promise.all(
+        Object.entities(woeids).map(async ([locale, id]) => {
+          try {
+            const result = await strapi.hook.twitter.getTrends(id)
 
-        if (!Array.isArray(result)) return
+            if (!Array.isArray(result)) return
 
-        if (!result[0]) return
+            if (!result[0]) return
 
-        const data = result[0].trends
+            const data = result[0].trends
 
-        const entity = await strapi.services.trend.createOrUpdate({
-          [locale]: data,
-        })
-        return sanitizeEntity(entity, { model: strapi.models.trend })
-      })
+            return strapi.services.trend.createOrUpdate({ [locale]: data })
+          } catch (error) {
+            console.error(error)
+          }
+        }),
+      )
+
+      entities.forEach(entity =>
+        sanitizeEntity(entity, { model: strapi.models.trend }),
+      )
     },
   },
 }
