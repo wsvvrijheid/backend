be9feaf4c48baeeb89bef5474a7f0ebed7d553db
7alip <talipaltas@gmail.com>
2022-03-04 20:38:21 +0100

    feat: ✨ add node tesseract

--- api/hashtag-post/models/hashtag-post.js
+++ api/hashtag-post/models/hashtag-post.js
@@ -1,8 +1,37 @@
 'use strict'
+const tesseract = require('node-tesseract-ocr')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {}
+const locales = {
+  nl: 'nld',
+  tr: 'tur',
+  en: 'eng',
+}
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(result) {
+      if (result.image) {
+        const imageUrl = result.image.url
+
+        try {
+          const text = await tesseract.recognize(
+            `http://localhost:1337${imageUrl}`,
+            {
+              lang: locales[result.locale],
+              oem: 3,
+              psm: 3,
+            },
+          )
+          await strapi.query('hashtag-post').update({ id: result.id }, { text })
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    },
+  },
+}
